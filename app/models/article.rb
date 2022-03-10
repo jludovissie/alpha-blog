6d3ead68c009c5813fbecfb178edcfe4c1e42506
Jordan Ludovissie <Jludovissie24@gmail.com>
2022-03-10 14:58:57 -0600

    first commit
    articles added

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+class Article < ApplicationRecord
+    #validations in the database 
+    validates :title, presence: true , length:{minimum: 6, maximum: 100}
+    validates :description, presence: true, length:{minimum: 10, maximum: 300}
+end
