d6a8c8db4602adf9953fe03b65cd59321c863e6a
Jordan Ludovissie <Jludovissie24@gmail.com>
2022-03-11 15:01:38 -0600

    Form Created
    CRU of Crud implemented
    
    Validation added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,40 @@
 class ArticlesController < ApplicationController
-    def show 
-      @article = Article.find(params[:id])  
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    # render plain: params[:article] -this just renders in browser
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    #render plain: @article.inspect
+    if @article.save
+      flash[:notice] = "Article was created sucessfully"
+      #redirect_to article_path(@article)
+      redirect_to @article
+    else
+      render "new"
     end
-    def index 
-      @articles = Article.all
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render "edit"
     end
+  end
 end
