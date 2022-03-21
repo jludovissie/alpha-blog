456921b2d28d6da75111122ab283fd172768991b
Jordan Ludovissie <Jludovissie24@gmail.com>
2022-03-21 15:19:59 -0500

    Clean up layout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -12,12 +13,11 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
     # render plain: params[:article] -this just renders in browser
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params) #@book = Book.new(params.require(:book).permit(:title,:description))
     #render plain: @article.inspect
     if @article.save
       flash[:notice] = "Article was created sucessfully"
@@ -29,12 +29,26 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully."
-      redirect_to @article
+      redirect_to @article #article_path
     else
-      render "edit"
+      render "edit" #render :new
     end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
