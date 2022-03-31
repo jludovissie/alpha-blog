0fe9a5bddfa0b375b820c1458594444f3a1e5a15
Jordan Ludovissie <Jludovissie24@gmail.com>
2022-03-31 14:57:42 -0500

    Added Admin Functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,13 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
   end
 
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 3)
   end
 
   def new
@@ -18,7 +20,7 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article] -this just renders in browser
     @article = Article.new(article_params) #@book = Book.new(params.require(:book).permit(:title,:description))
-    @article.user = User.first
+    @article.user = current_user 
     #render plain: @article.inspect
     if @article.save
       flash[:notice] = "Article was created sucessfully"
@@ -51,5 +53,12 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def require_same_user 
+    if current_user != @article.user && !current_user.admin?
+      flash[:alert] = "This is not your article"
+      redirect_to @article
+    end
   end
 end
